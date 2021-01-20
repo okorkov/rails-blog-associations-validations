d8c478b04715b56d6d650a899292f41f4053d0ee
okorkov <alex.okorkov@gmail.com>
2021-01-19 17:01:42 -0800

    commit tree 0896cb4ab3e4f14d74bb93285990ee6399c0cb9f

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,9 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
+    # raise post_params.inspect
     respond_to do |format|
+
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
@@ -69,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, tag_ids:[])
     end
 end
